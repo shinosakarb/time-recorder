e130adca9dae753a05d07420510bd21178bab0dc
techno-tanoC <develop.tanoc@gmail.com>
2016-06-25 16:23:32 +0900

    add members

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,8 +2,8 @@ import { handleActions } from 'redux-actions'
 
 const initialState = {
   members: [
-    {id: 1, author: "Pete Hunt", text: "This is one comment"},
-    {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+    {id: 1, name: "高垣 楓", checkin_time: "2016/06/25 09:00", checkout_time: "2016/06/25 15:00"},
+    {id: 2, name: "速水 奏", checkin_time: "2016/06/25 09:20", checkout_time: "2016/06/25 17:00"}
   ]
 }
 
