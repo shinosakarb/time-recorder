35eb408dcde58078ad76e98c6a5b5fdc7bbed1c2
chimame <rito.tamata@gmail.com>
2016-06-29 12:03:28 +0900

    Modify place the up work button by employees

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,9 @@ import { handleActions } from 'redux-actions'
 
 const initialState = {
   members: [
-    {id: 1, name: "高垣 楓", checkin_time: "2016/06/25 09:00", checkout_time: "2016/06/25 15:00"},
-    {id: 2, name: "速水 奏", checkin_time: "2016/06/25 09:20", checkout_time: "2016/06/25 17:00"}
-  ],
-  at_work: false
+    {id: 1, name: "高垣 楓", checkin_time: "2016/06/25 09:00", checkout_time: "2016/06/25 15:00", at_work: false},
+    {id: 2, name: "速水 奏", checkin_time: "2016/06/25 09:20", checkout_time: "2016/06/25 17:00", at_work: false}
+  ]
 }
 
 const reducerMap = {
@@ -16,9 +15,16 @@ const reducerMap = {
     return state.concat([action.payload])
   },
   up_work(state, action) {
+    let target_member = state.members.find((element, index, array) => {return element.id === action.payload})
+    let today = new Date()
+    let now   = `${today.getFullYear()}/${('0' + (today.getMonth() + 1)).slice(-2)}/${('0' + today.getDate()).slice(-2)} ${('0' + today.getHours()).slice(-2)}:${('0' + today.getMinutes()).slice(-2)}`
+    target_member.checkin_time  = (!target_member.at_work ? now : target_member.checkin_time)
+    target_member.checkout_time = (!target_member.at_work ? ""  : now)
+    target_member.at_work       = !target_member.at_work
+
     return {
       ...state,
-      at_work: !state.at_work
+      members: state.members.map((element) => {return (element.id === target_member.id ? target_member : element)})
     }
   }
 }
